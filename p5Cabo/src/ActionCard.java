d1af906aefefa8d41ff4dd731f9c960a5c8cb44b
hvsingh006 <167321894+hvsingh006@users.noreply.github.com>
2024-10-21 23:23:12 -0500

    Update ActionCard.java

--- p5Cabo/src/ActionCard.java
+++ p5Cabo/src/ActionCard.java
@@ -1,8 +1,32 @@
+//////////////// FILE HEADER (INCLUDE IN EVERY FILE) //////////////////////////
+//
+// Title:    Class designed to create cards along with the action associated with the card
+// Course:   CS 300 Fall 2024
+//
+// Author:   Harsh Singh
+// Email:    hvsingh@wisc.edu
+// Lecturer: Hobbes LeGault
+//
+//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ///////////////////
+// 
+// Partner Name:    Mohnish Nanthakumar
+// Partner Email:   mnanthakumar@wisc.edu
+// Partner Lecturer's Name: Hobbes LeGault
+// 
+// VERIFY THE FOLLOWING BY PLACING AN X NEXT TO EACH TRUE STATEMENT:
+//   X Write-up states that pair programming is allowed for this assignment.
+//   X We have both read and understand the course Pair Programming Policy.
+//   X We have registered our team prior to the team registration deadline.
+//
+//////////////////////// ASSISTANCE/HELP CITATIONS ////////////////////////////
+//
+// Persons:         NONE
+// Online Sources:  NONE
+
 /**
  * The ActionCard class represents a card that has an associated action.
  */
 public class ActionCard extends BaseCard {
-  // TODO: Whole class
   
   /**
    * The type of action associated with this card: "peek", "spy", or "switch".
@@ -15,9 +39,19 @@ public class ActionCard extends BaseCard {
    * @param rank the rank of the card (e.g., 1 for Ace, 13 for King).
    * @param suit the suit of the card (e.g., "Hearts", "Diamonds").
    * @param actionType the type of action associated with this card: "peek", "spy", or "switch"
+   * @throws IllegalArgumentException if actionType isn't "peek", "spy", or "switch" (added by us) 
    */
   public ActionCard(int rank, String suit, String actionType) {
+    // Call BaseCard constructor, so other variables are initialized (for example faceUp)
+    super(rank, suit);
     
+    // Initialize actionType if String is "peek", "spy", or "switch"
+    if (actionType.equalsIgnoreCase("peek") || actionType.equalsIgnoreCase("spy") 
+        || actionType.equalsIgnoreCase("switch")) {
+      this.actionType = actionType;
+    } else {
+      throw new IllegalArgumentException("Make sure that action type is peek, spy, or switch.");
+    }
   }
   
   /**
